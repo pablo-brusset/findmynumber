20ccce591c9f5d4579f07cd62d345c3968b346af
Pablo <pablo.brusset34@gmail.com>
2018-10-09 20:42:55 +0200

    First version

--- game.py
+++ game.py
@@ -1,21 +1,30 @@
 from random import randrange
 
-recommencer = True
-
 continuer = True
 
+recommencer = True
 while recommencer:
+
     nombre_a_trouver = randrange(101)
     # On choisit un nombre entre 1 et 100 au hasard et on le met dans nombre_a_trouver.
     print('L\'ordinateur a choisi')
 
     trouve = False
-
     while not trouve:
-        nombre_utilisateur = input('choisir un nombre ? ')
-        # On récupère une chaine de caractères
-        nombre_utilisateur = int(nombre_utilisateur)
-        print('L\'utilisateur a choisi', nombre_utilisateur)
+
+        nombre_utilisateur = -1
+        while nombre_utilisateur < 0:
+
+            nombre_utilisateur = input('choisir un nombre ? ')
+
+            print('L\'utilisateur a choisi', nombre_utilisateur)
+            try:
+                nombre_utilisateur = int(nombre_utilisateur)
+            except ValueError:
+                nombre_utilisateur = -1
+
+            if nombre_utilisateur < 0:
+                print('erreur, choisir un nouveau nombre superieur a 0')
 
         if nombre_utilisateur < nombre_a_trouver:
             print('C\'est plus')
@@ -25,8 +34,8 @@ while recommencer:
             print('C\'est gagné, bravo !')
             trouve = True
 
-    # On demande a l'utilisateur si il veut rejouer un partie.
     reponse = input('Recommencer ? o/N ')
+    # On demande a l'utilisateur si il veut rejouer un partie.
     if reponse == 'o' or reponse == 'O' or reponse == 'y' or reponse == 'Y':
         recommencer = True
     else:
