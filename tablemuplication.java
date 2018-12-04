ab80dba64a6c4ef169bea00d684c14c1479b7552
pablo-brusset <43478958+pablo-brusset@users.noreply.github.com>
2018-12-04 21:30:44 +0100

    Add files via upload

---
+++ tablemuplication.java
@@ -0,0 +1,15 @@
+package exercice;
+
+public class tablemuplication {
+	public static void main(String[] args) {
+		int trois = 3;
+		for(int i = 0; i<=10; i++ ) {
+			System.out.println(i + " multiplié par 3  est egal a "+ i*trois );
+			
+			
+			
+		}
+	}
+	
+
+}
