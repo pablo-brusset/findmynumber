c2780ec0266517dcfe55f7e56368c56fb26dfe7d
pablo-brusset <43478958+pablo-brusset@users.noreply.github.com>
2018-12-04 15:44:43 +0100

    Add files via upload

---
+++ exercie.java
@@ -0,0 +1,14 @@
+package exercice;
+
+import java.util.Scanner;
+
+public class exercie{
+	public static void main (String[]args ){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Saisissez votre prénom :");
+		String str = sc.nextLine();
+		System.out.println("Saisissez une chaine de caractere : " + str);
+		sc.close();	
+	}
+}
